4d4373d9fc1955837f991d8497ec23d23e2367e9
King12i <61169660+King12i@users.noreply.github.com>
2020-04-22 14:19:24 -0700

    This an Assagnment of Amadon

---
+++ poorly_coded_store/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Order, Product
+
+def index(request):
+    context = {
+        "all_products": Product.objects.all()
+    }
+    return render(request, "store/index.html", context)
+
+def checkout(request):
+    quantity_from_form = int(request.POST["quantity"])
+    price_from_form = float(request.POST["price"])
+    total_charge = quantity_from_form * price_from_form
+
+    context={
+        "price":str(price_from_form),
+        "item":str(quantity_from_form),
+        "charged":str(total_charge),
+    }
+
+    print("Charging credit card...")
+    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    return render(request, "store/checkout.html", context)
